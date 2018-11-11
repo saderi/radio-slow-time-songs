9f5a3d530d8f411f9e5c8215155d9f87160ec8c0
payam <saderi@gmail.com>
2018-11-11 22:34:13 +0300

    Set style for index page

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
     if ($result->num_rows > 0) {
         $lastPlay = array();
         while($row = $result->fetch_assoc()) {
-            $lastPlay[$row['id']]['name'] = $row["artist"].' - '.$row["title"];
+            $lastPlay[$row['id']]['song_title'] = $row["title"];
+            $lastPlay[$row['id']]['artist'] = $row["artist"];
             $lastPlay[$row['id']]['trackId'] = ($row["spotify_track_id"])? $row["spotify_track_id"] : '';
         }
     }
@@ -32,19 +33,43 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/css/foundation.min.css">
     </head>
     <body>
-        <div class="grid-x grid-padding-x">
-        <?php 
-            foreach ($lastPlay as $key => $value) { 
+        <div class="grid-container">
+            <p></p>            
+            <div class="callout">
+                <h1>10 last songs are played at <strong> Radio Slow Time </strong></h1>
+                <p>When I listen to <strong>Radio Slow Time</strong>, sometimes I want to add a specific song to my Spotify playlist. So I made this</p>
+                <a href="http://slowtime.com.tr/" class="font-bold">Radio Slow Time</a>
+            </div>
+            <?php 
+                foreach ($lastPlay as $key => $value) { 
 
-                $linkButton = ($value['trackId'])? '<a class="success button expanded" href="spotify:track:'.$value['trackId'].'">Add to Spotify</a>' : '<a class="alert button expanded" href="#">Search on Google</a>';
+                    $linkButton = ($value['trackId'])? '<a class="success button expanded" href="spotify:track:'.$value['trackId'].'">Add to Spotify</a>' : '<a class="alert button expanded" href="#">Search on Google</a>';
 
-                ?>
-                
-                <div class="cell medium-10 small-9"><?php echo $value['name']; ?></div>
-                <div class="cell medium-2 small-3"><?php  echo $linkButton; ?></div>
-        <?php } ?>
+                    ?>
+                    
+                    <div class="grid-x grid-padding-x align-middle">
+                        <div class="cell medium-10 small-9">
+                            <?php echo $value['song_title']; ?>
+                            <br />
+                            <small>
+                                 <strong><?php echo $value['artist']; ?></strong>
+                            </small>
+                            
+                                
+                            </div>
+                        <div class="cell medium-2 small-3"><?php  echo $linkButton; ?></div>
+                        <div class="cell small-12 text-center subheader">--------------------------</div>
+                    </div>
+            <?php } ?>
+
         </div>
-
+        <footer class="grid-container text-center">
+            <p> 
+                -------
+                <a href="https://github.com/saderi/radio-slow-time-songs">Fork this on GitHub</a>
+                -------
+            </p>
+        </footer>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/js/foundation.min.js"></script>
     </body>
